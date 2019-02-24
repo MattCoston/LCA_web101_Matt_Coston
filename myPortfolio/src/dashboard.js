2a36dc2c71cf4d761c343924bc6da09cb30c1e06
Matt Coston <mattcoston7@gmail.com>
2019-02-24 16:01:09 -0600

    Manipulating NASA.html

--- myPortfolio/src/dashboard.js
+++ myPortfolio/src/dashboard.js
@@ -1,14 +1,36 @@
 let name;
 let text;
 
-window.onload = function funkyFunc () {
+function funkyFunc() {
     name = prompt("Please enter your name: ");
-        if (name == '') {
-            document.getElementById('greeting');
-        }
-        else {
-            text = "Welcome " + name + " to my portfolio!";
-    document.getElementById('greeting').innerHTML = text 
-        } 
-    
+    if (name == '' || null) {
+        document.getElementById('greeting');
+    }
+    else {
+        text = "Welcome " + name + " to my portfolio!";
+        document.getElementById('greeting').innerHTML = text
+    }
+
 }
+funkyFunc();
+// color change function for generating three R G B values then changing the background color randomly
+
+function colorChange() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    var rgbColor = "rgb(" + r + "," + g + "," + b + ")";
+    console.log(rgbColor);
+
+    document.body.style.background = rgbColor;
+}
+
+
+// function that reloads the page
+
+function pageRefresh() {
+    location.reload();
+}
+// calls the colorChange function
+
+colorChange();
